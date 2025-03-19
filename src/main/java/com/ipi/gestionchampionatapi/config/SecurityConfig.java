eefdc496f617a53ee270431f08403545e225c201
OrjisDev <joris.paillasson@outlook.fr>
2025-03-19 14:40:07 +0100

    Patch shitty SecurityConfig

--- src/main/java/com/ipi/gestionchampionatapi/config/SecurityConfig.java
+++ src/main/java/com/ipi/gestionchampionatapi/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.ipi.gestionchampionatapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,8 +13,14 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests
-                -> authorizeRequests.anyRequest().permitAll());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
